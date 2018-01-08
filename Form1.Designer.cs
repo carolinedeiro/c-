2060fd2b51df6ff21cd19dc416f9576216dd813f
carolinedeiro <32653793+carolinedeiro@users.noreply.github.com>
2018-01-08 16:52:07 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace socorro
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ttxUsuario = new System.Windows.Forms.TextBox();
+            this.ttxSenha = new System.Windows.Forms.TextBox();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btEntrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ttxUsuario
+            // 
+            this.ttxUsuario.Location = new System.Drawing.Point(176, 113);
+            this.ttxUsuario.Name = "ttxUsuario";
+            this.ttxUsuario.Size = new System.Drawing.Size(192, 20);
+            this.ttxUsuario.TabIndex = 2;
+            this.ttxUsuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ttxSenha
+            // 
+            this.ttxSenha.Location = new System.Drawing.Point(176, 145);
+            this.ttxSenha.Name = "ttxSenha";
+            this.ttxSenha.Size = new System.Drawing.Size(192, 20);
+            this.ttxSenha.TabIndex = 3;
+            this.ttxSenha.UseSystemPasswordChar = true;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(176, 216);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btCancelar.TabIndex = 4;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btEntrar
+            // 
+            this.btEntrar.Location = new System.Drawing.Point(293, 216);
+            this.btEntrar.Name = "btEntrar";
+            this.btEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btEntrar.TabIndex = 5;
+            this.btEntrar.Text = "Entrar";
+            this.btEntrar.UseVisualStyleBackColor = true;
+            this.btEntrar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 320);
+            this.Controls.Add(this.btEntrar);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.ttxSenha);
+            this.Controls.Add(this.ttxUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ttxUsuario;
+        private System.Windows.Forms.TextBox ttxSenha;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btEntrar;
+    }
+}
+
